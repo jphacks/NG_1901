91114b51aabbe1b8273d420d16671a793dd6b016
洞口伊織 <iori@tp-iori-macbook.ics.nitech.ac.jp>
2019-10-19 17:58:14 +0900

    new file

---
+++ noti_purchase/list_other_picture.py
@@ -0,0 +1,22 @@
+import requests
+import re
+from bs4 import BeautifulSoup
+
+def serch_picture(serch_word):
+
+    url = "https://search.nifty.com/imagesearch/search?select=1&q=%s&ss=up"
+    keyword = serch_word
+    r = requests.get(url%(keyword))
+    soup = BeautifulSoup(r.text,'lxml')
+    imgs = soup.find_all('img',src=re.compile('^https://msp.c.yimg.jp/yjimage'))
+
+    #条件により検索ワードに近い画像が20個くらいに絞られています
+    #countにより無理やり一個めの要素だけ出力しています
+    count = 0
+    for img in imgs:
+        if count == 1:
+            break
+        print(img['src'])
+        count = count+1
+
+serch_picture("ティッシュ")
